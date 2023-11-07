0e513666a8c306699aab08c9e5048371b2d724e7
jamesdiffeycoding <jamesdiffey8@gmail.com>
2023-11-07 16:42:40 +0000

    UI

--- flashcard-app/src/Flashcards.jsx
+++ flashcard-app/src/Flashcards.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import "./Styles/flashcards.css";
+import data from "../db.json";
 
+function FlashcardItem() {
+    const [db, setDb] = useState(data);
+    console.log(db);
+    const flashcardItems = db.map((data) => {
+        <div className="flashcard-item">
+            <p className="flashcard-emoji">🤔</p>
+            <p className="flashcard-text">What is React?</p>
+        </div>;
+    });
+
+    return <>{flashcardItems}</>;
+}
 function Flashcards() {
     return (
         <div className="flashcard-grid">
-            <div className="flashcard-item">
-                <p className="flashcard-emoji">🤔</p>
-                <p className="flashcard-text">What is React?</p>
-            </div>
-            <p>Flashcard Component</p>
+            <FlashcardItem />
         </div>
     );
 }
