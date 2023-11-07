21584ac92a04257c1585f821910c86d0d91d7354
jamesdiffeycoding <jamesdiffey8@gmail.com>
2023-11-07 15:22:02 +0000

    form component complete

--- flashcard-app/src/Flashcards.jsx
+++ flashcard-app/src/Flashcards.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import "./Styles/flashcards.css";
 
 function Flashcards() {
-	return (
-		<>
-			<p>Flashcard Component</p>
-		</>
-	);
+    return (
+        <div className="flashcard-grid">
+            <div className="flashcard-item">
+                <p className="flashcard-emoji">🤔</p>
+                <p className="flashcard-text">What is React?</p>
+            </div>
+            <p>Flashcard Component</p>
+        </div>
+    );
 }
 
-export default Flashcards
+export default Flashcards;
--- flashcard-app/src/Form.jsx
+++ flashcard-app/src/Form.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
+import "./Styles/form.css";
 
 function Form() {
-	return (
-		<>
-			<p>Form Component</p>
-		</>
-	);
+    return (
+        <>
+            <span className="form-container">
+                <label htmlFor="form-question-box">Question: </label>
+                <input
+                    type="text"
+                    name="question-input"
+                    id="form-question-box"
+                ></input>
+                <label htmlFor="form-answer-box">Answer: </label>
+                <input
+                    type="text"
+                    name="answer-input"
+                    id="form-answer-box"
+                ></input>
+                <button>Add</button>
+            </span>
+        </>
+    );
 }
 
-export default Form
+export default Form;
