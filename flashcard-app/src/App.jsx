9f5683d85e9b442e6a86a50840a3313b242c0edd
James Robertson <jaarobertson92@gmail.com>
2023-11-07 14:36:20 +0000

    header & header style v1.0

--- flashcard-app/src/App.jsx
+++ flashcard-app/src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./Styles/App.css";
 import Flashcards from "./Flashcards";
 import Footer from "./Footer";
 import Form from "./Form";
