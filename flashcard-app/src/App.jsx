27df2eb0d48eea7ab88999d8fff3ea545cccc6b3
jjw9494 <joejameswalsh94@gmail.com>
2023-11-09 10:13:24 +0000

    added no cards and count

--- flashcard-app/src/App.jsx
+++ flashcard-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./Styles/App.css";
 import Flashcards from "./Flashcards";
@@ -8,15 +8,21 @@ import Header from "./Header";
 import data from "../db.json";
 
 function App() {
-    const [db, setDb] = useState(data);
-    return (
-        <>
-            <Header />
-            <Form setDb={setDb} />
-            <Flashcards db={db} setDb={setDb} />
-            <Footer />
-        </>
-    );
+	const [db, setDb] = useState(data);
+	const [count, setCount] = useState(db.length);
+
+	useEffect(() => {
+		setCount(db.length);
+	}, [db]);
+
+	return (
+		<>
+			<Header />
+			<Form setDb={setDb} />
+			<Flashcards db={db} setDb={setDb} />
+			<Footer count={count} />
+		</>
+	);
 }
 
 export default App;
--- flashcard-app/src/Flashcards.jsx
+++ flashcard-app/src/Flashcards.jsx
@@ -3,26 +3,33 @@ import "./Styles/flashcards.css";
 import Flashcard from "./Flashcard";
 
 function Flashcards({ db, setDb }) {
-    function deleteCard(e) {
-        let updatedArray = db.filter((x) => x.id != e.target.id);
-        setDb(updatedArray);
-    }
+	function deleteCard(e) {
+		let updatedArray = db.filter((x) => x.id != e.target.id);
+		setDb(updatedArray);
+	}
 
-    return (
-        <div>
-            <div className="flashcard-grid">
-                {db.map(({ id, question, answer }) => (
-                    <Flashcard
-                        key={Number(id)}
-                        question={question}
-                        answer={answer}
-                        deleteCard={deleteCard}
-                        id={Number(id)}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			{db.length == 0 ? (
+				<p className="flashcard-empty">
+					You haven't added any flashcards yet! Start creating your learning
+					set.
+				</p>
+			) : (
+				<div className="flashcard-grid">
+					{db.map(({ id, question, answer }) => (
+						<Flashcard
+							key={Number(id)}
+							question={question}
+							answer={answer}
+							deleteCard={deleteCard}
+							id={Number(id)}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Flashcards;
