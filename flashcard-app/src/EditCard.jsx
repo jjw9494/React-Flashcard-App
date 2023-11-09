019bbb5a360c0a9c7fdfc1ebac956eac228ecf6f
jjw9494 <joejameswalsh94@gmail.com>
2023-11-09 16:24:31 +0000

    created edit function

--- flashcard-app/src/EditCard.jsx
+++ flashcard-app/src/EditCard.jsx
@@ -1,59 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Styles/editcard.css";
 
 export default function EditCard({ updateCardWithText, idValue, db }) {
-    //text from editted question box
-    const [edittedQuestion, setEdittedQuestion] = useState("a");
-    //text from editted answer box
-    const [edittedAnswer, setEdittedAnswer] = useState("b");
-    function handleSubmit() {
-        updateCardWithText(edittedQuestion, edittedAnswer);
-    }
-    function updateQuestionState(e) {
-        setEdittedQuestion(e.target.value);
-    }
-    function updateAnswerState(e) {
-        setEdittedAnswer(e.target.value);
-    }
+	//text from editted question box
+	const [edittedQuestion, setEdittedQuestion] = useState("");
+	//text from editted answer box
+	const [edittedAnswer, setEdittedAnswer] = useState("");
 
-    return (
-        <>
-            <span className="edit-form-container">
-                <div className="edit-question-container">
-                    <label
-                        htmlFor="edit-form-question-box"
-                        className="edit-form-labels"
-                    >
-                        Question:{" "}
-                    </label>
-                    <input
-                        onChange={updateQuestionState}
-                        value={edittedQuestion}
-                        type="text"
-                        name="edit-question-input"
-                        id="edit-form-question-box"
-                    ></input>
-                </div>
+	useEffect(() => {
+		const objectValues = db.filter((item) => item.id == idValue);
+		console.log(objectValues);
+		setEdittedQuestion(objectValues[0].question);
+		setEdittedAnswer(objectValues[0].answer);
+	}, [db, idValue]);
 
-                <div className="edit-answer-container">
-                    <label
-                        htmlFor="edit-form-answer-box"
-                        className="edit-form-labels"
-                    >
-                        Answer:{" "}
-                    </label>
-                    <input
-                        onChange={updateAnswerState}
-                        value={edittedAnswer}
-                        type="text"
-                        name="edit-answer-input"
-                        id="edit-form-answer-box"
-                    ></input>
-                </div>
-                <button className="edit-button" onClick={handleSubmit}>
-                    Update
-                </button>
-            </span>
-        </>
-    );
+	function handleSubmit() {
+		updateCardWithText(edittedQuestion, edittedAnswer);
+	}
+	function updateQuestionState(e) {
+		setEdittedQuestion(e.target.value);
+	}
+	function updateAnswerState(e) {
+		setEdittedAnswer(e.target.value);
+	}
+
+	return (
+		<>
+			<span className="edit-form-container">
+				<div className="edit-question-container">
+					<label htmlFor="edit-form-question-box" className="edit-form-labels">
+						Question:
+					</label>
+					<textarea
+						onChange={updateQuestionState}
+						value={edittedQuestion}
+						type="text-field"
+						name="edit-question-input"
+						id="edit-form-question-box"
+					></textarea>
+				</div>
+
+				<div className="edit-answer-container">
+					<label htmlFor="edit-form-answer-box" className="edit-form-labels">
+						Answer:
+					</label>
+					<textarea
+						onChange={updateAnswerState}
+						value={edittedAnswer}
+						type="text-field"
+						name="edit-answer-input"
+						id="edit-form-answer-box"
+					></textarea>
+				</div>
+				<button className="edit-button" onClick={handleSubmit}>
+					Update
+				</button>
+			</span>
+		</>
+	);
 }
--- flashcard-app/src/Flashcards.jsx
+++ flashcard-app/src/Flashcards.jsx
@@ -1,70 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Styles/flashcards.css";
 import Flashcard from "./Flashcard";
 import EditCard from "./EditCard";
 
 function Flashcards({ db, setDb }) {
-    //toggle the screen
-    const [editState, setEditState] = useState(false);
-    //id value of the selected card
-    const [idValue, setIdValue] = useState("");
-    //placeholder edit text
-    const [placeholderQuestionText, setPlaceholderQuestionText] = useState("");
-    const [placeholderAnswerText, setPlaceholderAnswerText] = useState("");
+	//toggle the screen
+	const [editState, setEditState] = useState(false);
+	//id value of the selected card
+	const [idValue, setIdValue] = useState("");
 
-    function deleteCard(e) {
-        let updatedArray = db.filter((x) => x.id != e.target.id);
-        setDb(updatedArray);
-    }
-    function editCard(e) {
-        setIdValue(e.target.id);
-        let getQuestionText = db.filter((item) => item.id == idValue);
-        setEditState(true);
-    }
-    function updateCardWithText(question, answer) {
-        setEditState(false);
-        const placeholderDb = db.map((item) =>
-            item.id == idValue
-                ? (item = {
-                      id: idValue,
-                      question: question,
-                      answer: answer,
-                  })
-                : item
-        );
-        console.log(placeholderDb);
-        setDb(placeholderDb);
-    }
+	function deleteCard(e) {
+		let updatedArray = db.filter((x) => x.id != e.target.id);
+		setDb(updatedArray);
+	}
+	function editCard(e) {
+		setEditState(true);
+		setIdValue(e.target.id);
+	}
 
-    return (
-        <div>
-            <div className="flashcard-grid">
-                {editState ? (
-                    <EditCard
-                        updateCardWithText={updateCardWithText}
-                        idValue={idValue}
-                        db={db}
-                    />
-                ) : db.length == 0 ? (
-                    <p className="flashcard-empty">
-                        You haven't added any flashcards yet! Start creating
-                        your learning set.
-                    </p>
-                ) : (
-                    db.map(({ id, question, answer }) => (
-                        <Flashcard
-                            key={Number(id)}
-                            question={question}
-                            answer={answer}
-                            deleteCard={deleteCard}
-                            id={Number(id)}
-                            editCard={editCard}
-                        />
-                    ))
-                )}
-            </div>
-        </div>
-    );
+	function updateCardWithText(question, answer) {
+		setEditState(false);
+		const placeholderDb = db.map((item) =>
+			item.id == idValue
+				? (item = {
+						id: idValue,
+						question: question,
+						answer: answer,
+				  })
+				: item
+		);
+		setDb(placeholderDb);
+	}
+
+	return (
+		<div>
+			<div className={editState ? "flashcard-flex" : "flashcard-grid"}>
+				{editState ? (
+					<EditCard
+						updateCardWithText={updateCardWithText}
+						db={db}
+						idValue={idValue}
+					/>
+				) : db.length == 0 ? (
+					<p className="flashcard-empty">
+						You haven't added any flashcards yet! Start creating your learning
+						set.
+					</p>
+				) : (
+					db.map(({ id, question, answer }) => (
+						<Flashcard
+							key={Number(id)}
+							question={question}
+							answer={answer}
+							deleteCard={deleteCard}
+							id={Number(id)}
+							editCard={editCard}
+						/>
+					))
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Flashcards;
